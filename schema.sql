6f34968ee45cddc1762f65fe8ce4a886b75a4933
Elia Ahonen <elia.ahonen@hotmail.com>
2023-04-10 21:21:38 +0300

    schema.sql

--- schema.sql
+++ schema.sql
@@ -17,8 +17,8 @@ CREATE TABLE auto
 );
 
 INSERT INTO auto (merkki, malli, vuosi, rekno, tiedot, hinta, kuva)
-VALUES ("Hyundai", "Veloster", 2013, "1.6l Turbo", 12700, "http://impoliteorange.altervista.org/koulu/velo/IMG_3035-final.jpg")
-, ("Dodge", "Challenger Hellcat", 2018, "V8 Supercharged", 60000, "http://impoliteorange.altervista.org/koulu/velo/hellcat.jpg");
+VALUES ("Hyundai", "Veloster", 2013, "XOS-797", "1.6l Turbo", 12700, "http://impoliteorange.altervista.org/koulu/velo/IMG_3035-final.jpg")
+, ("Dodge", "Challenger Hellcat", 2018, "HOT-801", "V8 Supercharged", 60000, "http://impoliteorange.altervista.org/koulu/velo/hellcat.jpg");
 
 CREATE TABLE tila
 (tilaid BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY
@@ -35,10 +35,11 @@ id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY
 , kuvaus VARCHAR(200)
 , kustannukset BIGINT
 , autoid BIGINT
-, FOREIGN KEY (autonid) REFERENCES auto(autoid)
-, FOREIGN KEY (tilanid) REFERENCES tila(tilaid));
+, tilaid BIGINT
+, FOREIGN KEY (autoid) REFERENCES auto(autoid)
+, FOREIGN KEY (tilaid) REFERENCES tila(tilaid));
 
-INSERT INTO car (nimi, osat, kuvaus, kustannukset, autorekno, tilanid)
+INSERT INTO projekti (nimi, osat, kuvaus, kustannukset, autoid, tilaid)
 VALUES ("Teippausta", "Inozek Vampire Red, mattamustaa, kiiltävän mustaa autoteippiä", "Teipataan auton etulippa, sivut, takapaneeli, peilit ja spoikka", 500.00, 1, 1),
 ("Vanteet", "8x18 et40, 225/40/18", "Uudet kesävanteet", 380.00, 1, 1), ("Spoileri", "spoileri, teippiä", "Spoikan tilaus ja asennus", 570.00, 1, 1), ("Cold air intake", "K&N cold air intake kit", "Moottorin ilmansuodatinjärjestelmän vaihto", 400.00, 1, 3), ("Putkisto", "MagnaFlow 2017-2023 Dodge Challenger xMOD Series Cat-Back Performance Exhaust System", "sporttiputkisto", 3000.00, 2, 3);
 
@@ -56,6 +57,6 @@ VALUES ('Minna', 'user',
 '$2a$10$bMFMIA5G2G8xQIHn76g/ReT3Tg2D8TaxwYIhhAtbS6UBRz7p7wNbC', 'ADMIN');
 
 SELECT * FROM auto;
-SELECR * FROM tila;
+SELECT * FROM tila;
 SELECT * FROM projekti;
 SELECT * FROM kayttaja;
